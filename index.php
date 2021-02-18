8b84574794f92430f3273825b65c0c5d05625b3f
inshal <muhammadinshal6@gmailcom>
2021-02-18 17:54:31 +0500

    Files Changed

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php 
+// Get our helper functions
+require_once("inc/functions.php");
 
 // $requests = $_GET;
 // $hmac     = $_GET['hmac'];
@@ -10,12 +12,12 @@
 $token = "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";
 $shop  = "gallery-one-shop";
 
-echo $shop;
+//echo $shop;
 
-//$collectionList = shopify_call($token, $shop, "/admin/api/2021-01/custom_collections.json", array(), "GET");
-//$collectionList = json_decode($collectionList['response'], JSON_PRETTY_PRINT);
+$collectionList = shopify_call($token, $shop, "/admin/api/2021-01/custom_collections.json", array(), "GET");
+$collectionList = json_decode($collectionList['response'], JSON_PRETTY_PRINT);
 
-//print_r($collectionList);
+print_r($collectionList);
 
 
 
