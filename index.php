5f228fffc0e6b0fafd4be8dd1eac71f66165a396
inshal <muhammadinshal6@gmailcom>
2021-02-18 17:52:48 +0500

    Files Changed

--- index.php
+++ index.php
@@ -1,19 +1,21 @@
 <?php 
 
-$requests = $_GET;
-$hmac     = $_GET['hmac'];
+// $requests = $_GET;
+// $hmac     = $_GET['hmac'];
 
-$serializeArray = serialize($requests);
-$requests       = array_diff_key($requests, array('hmac' => ''));
-ksort($requests);
+// $serializeArray = serialize($requests);
+// $requests       = array_diff_key($requests, array('hmac' => ''));
+// ksort($requests);
 
 $token = "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";
 $shop  = "gallery-one-shop";
 
-$collectionList = shopify_call($token, $shop, "/admin/api/2021-01/custom_collections.json", array(), "GET");
+echo $shop;
+
+//$collectionList = shopify_call($token, $shop, "/admin/api/2021-01/custom_collections.json", array(), "GET");
 //$collectionList = json_decode($collectionList['response'], JSON_PRETTY_PRINT);
 
-print_r($collectionList);
+//print_r($collectionList);
 
 
 
