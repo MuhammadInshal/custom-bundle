97696abc66594d7fa6ee64587129815b5fb9c0ce
inshal <muhammadinshal6@gmailcom>
2021-02-18 20:42:10 +0500

    Files Changed

--- product_table.php
+++ product_table.php
@@ -1,17 +1,35 @@
-<?php
-$products = shopify_call($token, $shop, "/admin/api/2021-01/products.json", array(), "GET");
-$products = json_decode($products['response'], JSON_PRETTY_PRINT);
+<table>
+  <thead>
+    <tr>
+      <th colspan="2">Image</th>
+      <th colspan="2">Product</th>
+      <th>Action</th>
+    </tr>
+  </thead>
+  <tbody>
+  	<?php
+		$products = shopify_call($token, $shop, "/admin/api/2021-01/products.json", array(), "GET");
+		$products = json_decode($products['response'], JSON_PRETTY_PRINT);
 
-foreach ($products as $product) {
-	foreach ($product as $key => $value) {
-		$image = '';
+		foreach ($products as $product) {
+			foreach ($product as $key => $value) {
+				$image = '';
 
-		if (count($value['images']) > 0) {
-			$image = $value['images'][0]['src'];
+				if (count($value['images']) > 0) {
+					$image = $value['images'][0]['src'];
+				}
+
+				// echo "<img src='". $image ."' width='60'> <br />";
+				// echo $value['title'] . "<br />";
+				?>
+				<tr>
+			      <td><a href="#"><img width="35" height="35" alt="" src="<?php echo $image; ?>"></a></td>
+			      <td><a href="#"><?php echo $value['title']; ?></a></td>
+			      <td><button class="secondary icon-trash"></button></td>
+			    </tr>
+			    <?php
+			}
 		}
-
-		echo "<img src='". $image ."' width='60'> <br />";
-		echo $value['title'] . "<br />";
-	}
-}
-?>
+	?>
+  </tbody>
+</table>
