adce7671da635a412300effaccc0e8a31bc2a2f6
inshal <muhammadinshal6@gmailcom>
2021-02-18 18:43:36 +0500

    Files Changed

--- index.php
+++ index.php
@@ -25,7 +25,8 @@ foreach ($collects as $collect) {
 	foreach ($collect as $key => $value) {
 		$products = shopify_call($token, $shop, "/admin/api/2021-01/products/". $value['product_id'] .".json", array(), "GET");
 		$products = json_decode($products['response'], JSON_PRETTY_PRINT);
-		echo $products['product']['title'];
+		print_r($products);
+		// echo $products['product']['title'];
 	}
 }
 
