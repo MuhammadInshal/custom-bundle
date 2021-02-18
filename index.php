e95b4203289f2833daedcf1b474561b7315ba479
inshal <muhammadinshal6@gmailcom>
2021-02-18 17:40:57 +0500

    Files Changed

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
 <?php 
 
-$store = $_GET;
+$requests = $_GET;
+$hmac     = $_GET['hmac'];
+
+$serializeArray = serialize($requests);
+$requests       = array_diff_key($requests, array('hmac' => ''));
+ksort($requests);
+
+$token = "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";
+$shop  = "gallery-one-shop";
+
+$collectionList = shopify_call($token, $shop, "/admin/api/2021-01/custom_collections.json", array(), "GET");
+$collectionList = json_decode($collectionList['response'], JSON_PRETTY_PRINT);
+
+print_r($collectionList);
+
+
 
 // header("Location: install.php?shop=" . $store['shop']);
 // exit();
-
-echo "Done";
 
 ?>
