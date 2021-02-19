0576566c455b111e7356ff6bc8445c164152fc52
inshal <muhammadinshal6@gmailcom>
2021-02-19 14:41:10 +0500

    Changes made in index.php

--- index.php
+++ index.php
@@ -61,11 +61,12 @@ $shop  = "gallery-one-shop";
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title></title>
-	<link rel="stylesheet" href="css/style.css">
+	<!-- <link rel="stylesheet" href="css/style.css"> -->
+	<link rel="stylesheet" href="https://unpkg.com/@shopify/polaris@6.0.0/dist/styles.css"/>
 </head>
 <body>
 
-	<main>
+	<!-- <main>
 		<section>
 		  <article>
 		  	<div class="card has-sections">
@@ -80,7 +81,42 @@ $shop  = "gallery-one-shop";
 			</div>
 		  </article>
 		</section>
-	</main>
+	</main> -->
+<div>
+  <div class="Polaris-Page Polaris-Page--fullWidth">
+    <div class="Polaris-Page-Header Polaris-Page-Header--hasActionMenu Polaris-Page-Header--mobileView Polaris-Page-Header--noBreadcrumbs Polaris-Page-Header--mediumTitle">
+      <div class="Polaris-Page-Header__Row">
+        <div class="Polaris-Page-Header__TitleWrapper">
+          <div>
+            <div class="Polaris-Header-Title__TitleAndSubtitleWrapper">
+              <h1 class="Polaris-Header-Title">Orders</h1>
+            </div>
+          </div>
+        </div>
+        <div class="Polaris-Page-Header__RightAlign">
+          <div class="Polaris-Page-Header__Actions">
+            <div class="Polaris-ActionMenu Polaris-ActionMenu--rollup">
+              <div>
+                <div class="Polaris-ActionMenu-RollupActions__RollupActivator"><button class="Polaris-Button Polaris-Button--outline Polaris-Button--iconOnly" aria-label="Actions" type="button" tabindex="0" aria-controls="Polarispopover4" aria-owns="Polarispopover4" aria-expanded="false"><span class="Polaris-Button__Content"><span class="Polaris-Button__Icon"><span class="Polaris-Icon"><svg viewBox="0 0 20 20" class="Polaris-Icon__Svg" focusable="false" aria-hidden="true">
+                            <path d="M6 10a2 2 0 1 1-4.001-.001A2 2 0 0 1 6 10zm6 0a2 2 0 1 1-4.001-.001A2 2 0 0 1 12 10zm6 0a2 2 0 1 1-4.001-.001A2 2 0 0 1 18 10z"></path>
+                          </svg></span></span></span></button></div>
+              </div>
+            </div>
+            <div class="Polaris-Page-Header__PrimaryActionWrapper"><button class="Polaris-Button Polaris-Button--primary Polaris-Button--iconOnly" aria-label="Create order" type="button"><span class="Polaris-Button__Content"><span class="Polaris-Button__Icon"><span class="Polaris-Icon"><svg viewBox="0 0 20 20" class="Polaris-Icon__Svg" focusable="false" aria-hidden="true">
+                        <path d="M17 9h-6V3a1 1 0 1 0-2 0v6H3a1 1 0 1 0 0 2h6v6a1 1 0 1 0 2 0v-6h6a1 1 0 1 0 0-2z"></path>
+                      </svg></span></span></span></button></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="Polaris-Page__Content">
+      <p>Wide page content</p>
+    </div>
+  </div>
+  <div id="PolarisPortalsContainer">
+    <div data-portal-id="popover-Polarisportal2"></div>
+  </div>
+</div>
 	
 </body>
 </html>
