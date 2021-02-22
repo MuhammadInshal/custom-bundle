57d60d884ef48e75427220f7c241992e0ab0a3ab
inshal <muhammadinshal6@gmailcom>
2021-02-22 17:47:14 +0500

    Change made in index.php

--- index.php
+++ index.php
@@ -61,8 +61,8 @@ $shop  = "gallery-one-shop";
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title></title>
-	<!-- <link rel="stylesheet" href="css/style.css"> -->
-	<link rel="stylesheet" href="https://unpkg.com/@shopify/polaris@6.0.0/dist/styles.css"/>
+	<link rel="stylesheet" href="css/style.css">
+	<!-- <link rel="stylesheet" href="https://unpkg.com/@shopify/polaris@6.0.0/dist/styles.css"/> -->
 </head>
 <body>
 
